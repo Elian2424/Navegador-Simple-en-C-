6b4704f4162e781b420901d30f786a45fa0ed7e2
Elian2424 <20186573@itla.edu.do>
2021-06-17 22:49:38 -0400

    Agregar archivos de proyecto.

---
+++ navegador/Form2.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace navegador
+{
+    partial class navegador1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(navegador1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.urltext = new System.Windows.Forms.ToolStripTextBox();
+            this.buscarb = new System.Windows.Forms.ToolStripButton();
+            this.navegador = new System.Windows.Forms.WebBrowser();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripSplitButton();
+            this.nuevaVentanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descargaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton5,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton4,
+            this.toolStripButton3,
+            this.urltext,
+            this.buscarb,
+            this.toolStripButton6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(773, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // urltext
+            // 
+            this.urltext.AcceptsTab = true;
+            this.urltext.BackColor = System.Drawing.Color.Gainsboro;
+            this.urltext.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.urltext.Name = "urltext";
+            this.urltext.Size = new System.Drawing.Size(590, 25);
+            this.urltext.Click += new System.EventHandler(this.urltext_Click);
+            // 
+            // buscarb
+            // 
+            this.buscarb.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.buscarb.Image = ((System.Drawing.Image)(resources.GetObject("buscarb.Image")));
+            this.buscarb.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buscarb.Name = "buscarb";
+            this.buscarb.Size = new System.Drawing.Size(23, 22);
+            this.buscarb.Text = "toolStripButton6";
+            this.buscarb.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // navegador
+            // 
+            this.navegador.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.navegador.Location = new System.Drawing.Point(0, 25);
+            this.navegador.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.navegador.MinimumSize = new System.Drawing.Size(20, 20);
+            this.navegador.Name = "navegador";
+            this.navegador.Size = new System.Drawing.Size(773, 425);
+            this.navegador.TabIndex = 2;
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaVentanaToolStripMenuItem,
+            this.historialToolStripMenuItem,
+            this.descargaToolStripMenuItem,
+            this.configuracionToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(32, 20);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.ButtonClick += new System.EventHandler(this.toolStripButton6_ButtonClick);
+            // 
+            // nuevaVentanaToolStripMenuItem
+            // 
+            this.nuevaVentanaToolStripMenuItem.Name = "nuevaVentanaToolStripMenuItem";
+            this.nuevaVentanaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevaVentanaToolStripMenuItem.Text = "Nueva Ventana";
+            // 
+            // historialToolStripMenuItem
+            // 
+            this.historialToolStripMenuItem.Name = "historialToolStripMenuItem";
+            this.historialToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.historialToolStripMenuItem.Text = "Historial";
+            // 
+            // descargaToolStripMenuItem
+            // 
+            this.descargaToolStripMenuItem.Name = "descargaToolStripMenuItem";
+            this.descargaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.descargaToolStripMenuItem.Text = "Descarga";
+            // 
+            // configuracionToolStripMenuItem
+            // 
+            this.configuracionToolStripMenuItem.Name = "configuracionToolStripMenuItem";
+            this.configuracionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configuracionToolStripMenuItem.Text = "Configuracion";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click_1);
+            // 
+            // navegador1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 450);
+            this.Controls.Add(this.navegador);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "navegador1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Liamp";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripTextBox urltext;
+        private System.Windows.Forms.ToolStripButton buscarb;
+        private System.Windows.Forms.WebBrowser navegador;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripSplitButton toolStripButton6;
+        private System.Windows.Forms.ToolStripMenuItem nuevaVentanaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descargaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
---
+++ navegador/Form2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace navegador
+{
+    public partial class navegador1 : Form
+    {
+        public navegador1()
+        {
+            InitializeComponent();
+            navegador.GoHome();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+           navegador.Navigate(urltext.Text);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            navegador.GoBack();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            navegador.GoForward();
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            navegador.Stop();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            navegador.Refresh();
+
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            navegador.GoHome();
+        }
+
+        private void urltext_Click(object sender, EventArgs e)
+        {
+            //referencia a la propiedad AcceptButton
+
+            
+
+             
+            
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripButton7_ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton6_ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
